8011f82a7767edbbbfa4dabb0f9d045ab6669140
rkr35 <rkr35@drexel.edu>
2020-05-02 16:28:51 -0400

    Initialize console and logger

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
+use std::io::{self, Read};
+use std::panic;
 use std::ptr;
+
+use log::info;
+use simplelog::{Config, LevelFilter, TermLogger, TerminalMode};
 use wchar::wch_c as w;
 use winapi::{
     um::{
@@ -11,7 +16,7 @@ use winapi::{
     },
 
     shared::{
-        minwindef::{BOOL, DWORD, FALSE, HINSTANCE, LPVOID, TRUE},
+        minwindef::{BOOL, DWORD, HINSTANCE, LPVOID, TRUE},
     }
 };
 
@@ -21,10 +26,32 @@ fn msg_box(text: &[u16], caption: &[u16]) {
     unsafe { MessageBoxW(ptr::null_mut(), text.as_ptr(), caption.as_ptr(), MB_OK); }
 }
 
+fn idle() {
+    info!("Idling. Press enter to continue.");
+    let mut sentinel = [0; 2];
+    let _ = io::stdin().read_exact(&mut sentinel);
+}
+
 extern "system" fn on_attach(dll: LPVOID) -> DWORD {
     msg_box(w!("Okay."), w!("on_attach"));
 
-    unsafe { AllocConsole() };
+    let result = panic::catch_unwind(|| {
+        unsafe { AllocConsole() };
+        println!("Allocated console.");
+
+        if let Err(e) = TermLogger::init(LevelFilter::Info, Config::default(), TerminalMode::Mixed) {
+            eprintln!("Failed to initialize logger: {}", e);
+        } else {
+            info!("Initialized logger.");
+        }
+
+        idle();
+    });
+
+    if result.is_err() {
+        let text = w!("on_attach() caught a panic. The state of the hook is unknown. The hook will now detach.");
+        msg_box(text, w!("Detaching because of a panic."));
+    }
 
     unsafe {
         FreeConsole();
