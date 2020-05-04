cd1fe6e97ae8a342fbeefb07f74fb2372475d071
rkr35 <rkr35@drexel.edu>
2020-05-04 19:35:43 -0400

    Remove debug comments

--- src/vgui.rs
+++ src/vgui.rs
@@ -1,10 +1,4 @@
 #[repr(C)]
 pub struct Panel {
-    pub vtable: *mut usize, // 0x683ac2e4
+    pub vtable: *mut usize,
 }
-
-// vtable entry address: panel.vtable + 0x2c
-// function pointer = (*vtable entry address) cast
-
-// vtable entry address: vtable.add(11) = 0x683AC310
-// *vtable.add(11) = *0x683AC310 = 0x6834D620 
