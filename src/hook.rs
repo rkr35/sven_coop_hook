2f9e8c18cc2d7049e4572cba6f33dedff079d1a7
rkr35 <rkr35@drexel.edu>
2020-05-05 18:55:16 -0400

    Hook Panel vtable by swapping

--- src/hook.rs
+++ src/hook.rs
@@ -8,11 +8,12 @@ use log::{error, info};
 
 struct Hook<'a> {
     _panel: &'a mut Panel,
-    _paint_traverse_hook: Patch<usize>,
+    panel_vtable: Patch<*mut usize>,
 }
 
 impl<'a> Drop for Hook<'a> {
     fn drop(&mut self) {
+        unsafe { self.panel_vtable.restore(); }
         info!("Hook dropped.");
     }
 }
@@ -33,8 +34,7 @@ impl Modules {
 
 static mut OLD_PAINT_TRAVERSE: usize = 0;
 extern "fastcall" fn my_paint_traverse(this: usize, edx: usize, panel: usize, force_repaint: bool, allow_force: bool) {
-    type PaintTraverseFn = extern "fastcall" fn(usize, usize, usize, bool, bool);
-    let original: PaintTraverseFn = unsafe { mem::transmute(OLD_PAINT_TRAVERSE) };
+    let original: extern "fastcall" fn(usize, usize, usize, bool, bool) = unsafe { mem::transmute(OLD_PAINT_TRAVERSE) };
     original(this, edx, panel, force_repaint, allow_force);
 }
 
@@ -54,7 +54,7 @@ fn hook_and_idle(modules: &Modules) -> Result<(), Error> {
     const PANEL_INTERFACE: &str = "VGUI_Panel007";
 
     #[repr(usize)]
-    enum PanelVtableIndex {
+    enum PanelVtable {
         PaintTraverse = 41,
         Max = 60
     }
@@ -66,27 +66,47 @@ fn hook_and_idle(modules: &Modules) -> Result<(), Error> {
         return Err(Error::NullVTable(PANEL_INTERFACE));
     }
 
-    let mut vtable_copy = [0; PanelVtableIndex::Max as _];
+    let mut modified_vtable = {
+        // Create storage for our vtable copy.
+        let mut vtable = [0; PanelVtable::Max as _];
+        
+        // Copy the original Panel vtable to our vtable.
+        unsafe { panel.vtable.copy_to_nonoverlapping(vtable.as_mut_ptr(), PanelVtable::Max as _); }
 
-    // Important: vtable_copy must outlive vtable_copy.as_mut_ptr().
-    // Otherwise the pointer will dangle.
-    unsafe { panel.vtable.copy_to_nonoverlapping(vtable_copy.as_mut_ptr(), PanelVtableIndex::Max as _); }
+        // Hook PaintTraverse.
+        vtable[PanelVtable::PaintTraverse as usize] = my_paint_traverse as usize;
 
-    vtable_copy[PanelVtableIndex::PaintTraverse as usize] = my_paint_traverse as usize;
-
-    let patch = Patch::new(unsafe { panel.vtable.add(41) }, my_paint_traverse as usize).unwrap();
-    unsafe { OLD_PAINT_TRAVERSE = patch.old_value; }
-
-    let _hook = Hook {
-        _panel: panel,
-        _paint_traverse_hook: patch
+        vtable
     };
 
-    idle();
-    Ok(())
+    // Set Panel's vtable to our vtable which contains the PaintTraverse hook.
+    // SAFETY: You must ensure that `modified_vtable` outlives `modified_vtable.as_mut_ptr()`.
+    // Otherwise this mutable pointer will be a dangling reference. The easiest way to satisfy
+    // this safety requirement is to not move `modified_vtable` while this patch is alive.
+    let patch = unsafe {
+        // TODO: Gracefully handle `.unwrap()`
+        let p = Patch::new(&mut panel.vtable, modified_vtable.as_mut_ptr()).unwrap();
+        
+        // Save the address of the original PaintTraverse function, so we can call it in our hook.
+        // TODO: Gracefully handle null vtable entry.
+        OLD_PAINT_TRAVERSE = *p.old_value.add(PanelVtable::PaintTraverse as usize);
 
-    // `_hook` is automatically dropped here.
-    // Any cleanup logic for Hook should be specified in its implementation of Drop.
+        p
+    };
+
+    {
+        let _hook = Hook {
+            _panel: panel,
+            panel_vtable: patch
+        };
+
+        idle();
+
+        // `_hook` dropped here.
+        // Any cleanup logic, e.g., restoring patches, should be done in Hook's implementation of Drop.
+    }
+
+    Ok(())
 }
 
 pub fn run() {
