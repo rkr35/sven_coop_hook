ded836e5e9f83925a8fd23c983b5a2e25256da7e
rkr35 <rkr35@drexel.edu>
2020-05-05 18:54:39 -0400

    Add unsafe qualifiers on Patch
    These operations are unsafe or transitively unsafe, i.e., for all
    given `address` of type `*mut T`, there's no guarantee that the
    pointer is valid (non-null, aligned). It's up to the caller to
    ensure that their particular inputs will work safely with Patch.

--- src/memory.rs
+++ src/memory.rs
@@ -11,14 +11,14 @@ pub struct Patch<T> where T: Copy + Clone {
 }
 
 impl<T> Patch<T> where T: Copy + Clone {
-    pub fn new(address: *mut T, new_value: T) -> Option<Self> {
+    pub unsafe fn new(address: *mut T, new_value: T) -> Option<Self> {
         Some(Self {
             address,
             old_value: Self::patch(address, new_value)?
         })
     }
 
-    fn patch(pointer: *mut T, new_value: T) -> Option<T> {
+    unsafe fn patch(pointer: *mut T, new_value: T) -> Option<T> {
         if pointer.is_null() {
             return None;
         }
@@ -26,19 +26,16 @@ impl<T> Patch<T> where T: Copy + Clone {
         let old_value;
         let mut old_protection = 0;
 
-        unsafe {
-            // Save the previous value at this address.
-            old_value = *pointer;
+        // Save the previous value at this address.
+        old_value = *pointer;
 
-            // Change page protection and write new value.
-            VirtualProtect(pointer.cast(), mem::size_of::<T>(), PAGE_EXECUTE_READWRITE, &mut old_protection);
-            *pointer = new_value;
-            VirtualProtect(pointer.cast(), mem::size_of::<T>(), old_protection, &mut old_protection);
-        }
+        // Change page protection and write new value.
+        VirtualProtect(pointer.cast(), mem::size_of::<T>(), PAGE_EXECUTE_READWRITE, &mut old_protection);
+        *pointer = new_value;
+        VirtualProtect(pointer.cast(), mem::size_of::<T>(), old_protection, &mut old_protection);
 
         Some(old_value)
     }
-}
 
     pub unsafe fn restore(&self) {
         Self::patch(self.address, self.old_value);
