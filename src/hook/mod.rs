a2113cdee25d441885eabd69944e242adfb72902
rkr35 <rkr35@drexel.edu>
2020-05-09 18:05:17 -0400

    Add CL_CreateMove

--- src/hook/mod.rs
+++ src/hook/mod.rs
@@ -8,6 +8,7 @@ use std::ptr;
 
 use log::{error, info};
 use thiserror::Error;
+use ultraviolet::Vec3 as Vector;
 
 mod panel;
 
@@ -94,6 +95,7 @@ impl EngineFuncs {
 
 #[repr(usize)]
 pub enum ClientFuncsTable {
+    CreateMove = 15,
     NumEntries = 43,
 }
 
@@ -101,6 +103,32 @@ pub enum ClientFuncsTable {
 #[derive(Clone)]
 pub struct ClientFuncs {
     functions: [usize; ClientFuncsTable::NumEntries as usize]
+}
+
+impl ClientFuncs {
+    // void(*CL_CreateMove) (float frametime, struct usercmd_s *cmd, int active);
+    pub fn create_move(&self, frame_time: f32, cmd: *mut UserCmd, active: i32) {
+        type CreateMove = extern "C" fn(frame_time: f32, cmd: *mut UserCmd, active: i32);
+        let address = self.functions[ClientFuncsTable::CreateMove as usize];
+        let function: CreateMove = unsafe { mem::transmute(address) };
+        function(frame_time, cmd, active);
+    }
+}
+
+#[repr(C)]
+pub struct UserCmd {
+    lerp_msec: i16,
+    msec: u8,
+    view_angles: Vector,
+    forward_move: f32,
+    side_move: f32,
+    up_move: f32,
+    light_level: u8,
+    buttons: u16,
+    impulse: u8,
+    weapon_select: u8,
+    impact_index: i32,
+    impact_position: Vector,
 }
 
 fn get_screen_fade_instruction(hw: &Module) -> Result<*const u8, Error<'static>> {
