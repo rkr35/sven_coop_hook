4d53155b29064f4115282815ce9f09c98da3d4ac
rkr35 <rkr35@drexel.edu>
2020-05-09 19:03:45 -0400

    Implement RAII client hook

--- src/hook/client.rs
+++ src/hook/client.rs
@@ -7,25 +7,29 @@ use crate::hook::ORIGINAL_CLIENT_FUNCS;
 // END MUTABLE GLOBAL STATE
 
 pub struct Hook {
-
+    client_funcs: *mut ClientFuncs,
 }
 
 impl Hook {
-    pub unsafe fn new(client_funcs: *mut ClientFuncs) -> Self {
-        Self {
+    pub fn new(client_funcs: *mut ClientFuncs) -> Self {
+        unsafe {
+            (*client_funcs).hook(ClientFuncsTable::CreateMove, my_create_move as usize);
+        }
 
+        Self {
+            client_funcs
         }
     }
 }
 
 impl Drop for Hook {
     fn drop(&mut self) {
+        unsafe {
+            *(self.client_funcs) = (*ORIGINAL_CLIENT_FUNCS.as_ptr()).clone()
+        }
 
+        info!("Client hook dropped.");
     }
-}
-
-pub unsafe fn hook(client_funcs: *mut ClientFuncs) {
-    (*client_funcs).hook(ClientFuncsTable::CreateMove, my_create_move as usize);
 }
 
 extern "C" fn my_create_move(frame_time: f32, cmd: *mut UserCmd, active: i32) {
@@ -33,5 +37,5 @@ extern "C" fn my_create_move(frame_time: f32, cmd: *mut UserCmd, active: i32) {
         (*ORIGINAL_CLIENT_FUNCS.as_ptr()).create_move(frame_time, cmd, active)
     }
 
-    info!("my_create_move()");
+    info!("fps = {}", 1.0 / frame_time);
 }
