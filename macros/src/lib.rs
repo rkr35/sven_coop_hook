66b3c9a212f1c04c29f371594b88065ebcffa313
rkr35 <rkr35@drexel.edu>
2020-05-10 16:29:40 -0400

    Add initial implementation of `vtable!`

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -6,11 +6,17 @@ use syn::{Ident, LitInt, parenthesized, parse_macro_input, Token, Type, Visibili
 use syn::parse::{Parse, ParseStream, Result};
 use syn::punctuated::Punctuated;
 
+enum FunctionKind {
+    Regular,
+    Virtual,
+}
+
 struct Function {
     index: LitInt,
     visibility: Visibility,
     name: Ident,
     return_type: Type,
+    kind: FunctionKind,
 }
 
 impl Parse for Function {
@@ -44,19 +50,24 @@ impl Parse for Function {
             visibility,
             name,
             return_type,
+            kind: FunctionKind::Regular,
         })
     }
 }
 
 impl ToTokens for Function {
     fn to_tokens(&self, tokens: &mut TokenStream) {
-        let Function { index, visibility, name, return_type } = &self;
+        let Function { index, visibility, name, return_type, kind } = &self;
+        
+        let address = match kind {
+            FunctionKind::Regular => quote! { self.functions[#index] },
+            FunctionKind::Virtual => quote! { (*self.vtable)[#index] },
+        };
 
         *tokens = quote! {
             #visibility fn #name(&self) -> #return_type {
                 type Function = extern "C" fn() -> #return_type;
-                let address = self.functions[#index];
-                let function = unsafe { core::mem::transmute::<usize, Function>(address) };
+                let function = unsafe { core::mem::transmute::<usize, Function>(#address) };
                 function()
             }
         };
@@ -75,6 +86,22 @@ impl Parse for Functions {
     }
 }
 
+struct Vtable {
+    entries: Punctuated<Function, Token![,]>,
+}
+
+impl Parse for Vtable {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(Self {
+            entries: input.parse_terminated(|p| {
+                let mut entry = Function::parse(p)?;
+                entry.kind = FunctionKind::Virtual;
+                Ok(entry)
+            })?,
+        })
+    }
+}
+
 #[proc_macro]
 pub fn functions(input: OldTokenStream) -> OldTokenStream {
     let Functions { entries } = parse_macro_input!(input as Functions);
@@ -83,6 +110,20 @@ pub fn functions(input: OldTokenStream) -> OldTokenStream {
         .into_iter()
         .map(ToTokens::into_token_stream)
         .collect();
+
+    generated.into()
+}
+
+#[proc_macro]
+pub fn vtable(input: OldTokenStream) -> OldTokenStream {
+    let Vtable { entries } = parse_macro_input!(input as Vtable);
+    
+    let generated: TokenStream = entries
+        .into_iter()
+        .map(ToTokens::into_token_stream)
+        .collect();
+
+    println!("{}", generated);
 
     generated.into()
 }
