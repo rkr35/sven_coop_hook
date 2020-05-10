fd49619eae79f25f8ac38b7e42e519cad2e40300
rkr35 <rkr35@drexel.edu>
2020-05-10 10:30:13 -0400

    Refactor client hook

--- src/hook/client.rs
+++ src/hook/client.rs
@@ -2,10 +2,12 @@ use crate::game::client::{ClientFuncs, ClientFuncsTable, RefParams, UserCmd};
 use crate::memory;
 
 use log::info;
+use ultraviolet::Vec3 as Vector;
 
 // BEGIN MUTABLE GLOBAL STATE
 use crate::hook::ORIGINAL_CLIENT_FUNCS;
 use crate::hook::PLAYER_MOVE;
+static mut RECOIL_ANGLE: Vector = Vector { x: 0.0, y: 0.0, z: 0.0 };
 // END MUTABLE GLOBAL STATE
 
 pub struct Hook {
@@ -35,49 +37,39 @@ impl Drop for Hook {
     }
 }
 
-fn bunny_hop(cmd: *mut UserCmd) {
+unsafe fn bunny_hop(cmd: *mut UserCmd) {
+    const IN_JUMP: u16 = 1 << 1;
+    const FL_ONGROUND: i32 = 1 << 9;
+
+    if (*cmd).buttons & IN_JUMP != IN_JUMP {
+        return;
+    }
+
+    (*cmd).buttons &= !IN_JUMP;
+
+    let on_ground = (*PLAYER_MOVE).flags & FL_ONGROUND == FL_ONGROUND;
+    
+    if on_ground || (*PLAYER_MOVE).water_level >= 2 {
+        (*cmd).buttons |= IN_JUMP;
+    }
+}
+
+unsafe extern "C" fn my_create_move(frame_time: f32, cmd: *mut UserCmd, active: i32) {
+    ORIGINAL_CLIENT_FUNCS.as_ref().unwrap().create_move(frame_time, cmd, active);
+
     if memory::ptr_check(cmd).is_err() {
         return;
     }
 
-    unsafe {
-        const IN_JUMP: u16 = 1 << 1;
-        const FL_ONGROUND: i32 = 1 << 9;
-
-        if (*cmd).buttons & IN_JUMP != IN_JUMP {
-            return;
-        }
-
-        (*cmd).buttons &= !IN_JUMP;
-
-        let on_ground = (*PLAYER_MOVE).flags & FL_ONGROUND == FL_ONGROUND;
-        
-        if on_ground || (*PLAYER_MOVE).water_level >= 2 {
-            (*cmd).buttons |= IN_JUMP;
-        }
-    }
-}
-
-fn no_recoil(cmd: *mut UserCmd) {
-
-}
-
-extern "C" fn my_create_move(frame_time: f32, cmd: *mut UserCmd, active: i32) {
-    unsafe {
-        ORIGINAL_CLIENT_FUNCS.as_ref().unwrap().create_move(frame_time, cmd, active);
-    }
-
     bunny_hop(cmd);
-    no_recoil(cmd);
+    (*cmd).view_angles = Vector::zero();
 }
 
 // void(*V_CalcRefdef) (struct ref_params_s *pparams);
-extern "C" fn my_calc_ref_def(params: *mut RefParams) {
-    unsafe {
-        ORIGINAL_CLIENT_FUNCS.as_ref().unwrap().calc_ref_def(params);
-    }
-
-    if memory::ptr_check(params).is_ok() {
-        info!("view_angles = {:?}", unsafe { (*params).view_angles });
+unsafe extern "C" fn my_calc_ref_def(params: *mut RefParams) {
+    ORIGINAL_CLIENT_FUNCS.as_ref().unwrap().calc_ref_def(params);
+    
+    if memory::ptr_check(params).is_err() {
+        return;
     }
 }
