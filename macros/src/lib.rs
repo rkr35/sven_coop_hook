0a402cd54ccc4b4c8611d7452fb5cd51377491ae
rkr35 <rkr35@drexel.edu>
2020-05-10 15:25:02 -0400

    Implement first pass of `functions!` macro
    Only works for functions entries with no arguments.

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -1,18 +1,83 @@
-use proc_macro::TokenStream;
+use proc_macro::TokenStream as OldTokenStream;
 
+use proc_macro2::TokenStream;
+use quote::{quote, ToTokens};
+use syn::{Ident, LitInt, parenthesized, parse_macro_input, Token, Type};
 use syn::parse::{Parse, ParseStream, Result};
+use syn::punctuated::Punctuated;
+
+struct Function {
+    index: LitInt,
+    name: Ident,
+    return_type: Type,
+}
+
+impl Parse for Function {
+    fn parse(input: ParseStream) -> Result<Self> {
+        // 51: get_local_player() -> *const Entity
+
+        // 51
+        let index = input.parse()?;
+        
+        // :
+        input.parse::<Token![:]>()?;
+        
+        // get_local_player
+        let name = input.parse()?;
+        
+        // ()
+        let _args;
+        parenthesized!(_args in input);
+
+        // ->
+        input.parse::<Token![->]>()?;
+
+        // *const Entity
+        let return_type = input.parse()?;
+
+        Ok(Self {
+            index,
+            name,
+            return_type,
+        })
+    }
+}
+
+impl ToTokens for Function {
+    fn to_tokens(&self, tokens: &mut TokenStream) {
+        let Function { index, name, return_type } = &self;
+
+        *tokens = quote! {
+            pub fn #name(&self) -> #return_type {
+                type Function = extern "C" fn() -> #return_type;
+                let address = self.functions[#index];
+                let function = unsafe { core::mem::transmute::<usize, Function>(address) };
+                function()
+            }
+        };
+    }
+}
 
 struct Functions {
-    
+    functions: Punctuated<Function, Token![,]>,
 }
 
 impl Parse for Functions {
     fn parse(input: ParseStream) -> Result<Self> {
-        todo!()
+        Ok(Self {
+            functions: input.parse_terminated(Function::parse)?,
+        })
     }
 }
 
 #[proc_macro]
-pub fn functions(input: TokenStream) -> TokenStream {
-    todo!()
+pub fn functions(input: OldTokenStream) -> OldTokenStream {
+    let Functions { functions } = parse_macro_input!(input as Functions);
+    
+    let generated: TokenStream = functions
+        .into_iter()
+        .map(ToTokens::into_token_stream)
+        .collect();
+
+    generated.into()
 }
--- src/game/engine.rs
+++ src/game/engine.rs
@@ -1,31 +1,17 @@
 use crate::game::client::Entity;
+use sven_coop_hook_macros::functions;
 
-use std::mem;
-
-#[repr(usize)]
-pub enum EngineFuncsTable {
-    GetLocalPlayer = 51,
-    NumEntries = 131,
-}
+const NUM_ENGINE_FUNCS: usize = 131;
 
 #[repr(C)]
 #[derive(Clone)]
 pub struct EngineFuncs {
-    functions: [usize; EngineFuncsTable::NumEntries as usize]
+    functions: [usize; NUM_ENGINE_FUNCS]
 }
 
 impl EngineFuncs {
-    // functions! {
-    //     36: get_max_clients() -> i32,
-    //     51: get_local_player() -> *const Entity,
-
-    // }
-
-    // struct cl_entity_s			*( *GetLocalPlayer )		( void );
-    pub fn get_local_player(&self) -> *const Entity {
-        type GetLocalPlayer = extern "C" fn() -> *const Entity;
-        let address = self.functions[EngineFuncsTable::GetLocalPlayer as usize];
-        let function: GetLocalPlayer = unsafe { mem::transmute(address) };
-        function()
+    functions! {
+        36: get_max_clients() -> i32,
+        51: get_local_player() -> *const Entity,
     }
 }
