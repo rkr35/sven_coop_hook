1b8e23585fa6d077b46a90c3227adcf0e24524a1
rkr35 <rkr35@drexel.edu>
2020-05-12 11:11:48 -0400

    Add bindgen to generate Rust bindings to SDK

---
+++ build.rs
@@ -0,0 +1,55 @@
+use std::env;
+use std::path::PathBuf;
+
+use bindgen::{Builder, CargoCallbacks, EnumVariation};
+
+fn main() {
+    // Tell cargo to invalidate the built crate whenever the wrapper changes
+    println!("cargo:rerun-if-changed=wrapper.hpp");
+
+    // The bindgen::Builder is the main entry point
+    // to bindgen, and lets you build up options for
+    // the resulting bindings.
+    let bindings = Builder::default()
+        // The input header we would like to generate
+        // bindings for.
+        .header("sdk/wrapper.hpp")
+        // Tell cargo to invalidate the built crate whenever any of the
+        // included header files changed.
+        .parse_callbacks(Box::new(CargoCallbacks))
+        // Don't generate layout tests.
+        .layout_tests(false)
+        // Don't generate derives.
+        .derive_copy(false)
+        .derive_debug(false)
+        .derive_default(false)
+        .derive_eq(false)
+        .derive_hash(false)
+        .derive_ord(false)
+        .derive_partialeq(false)
+        .derive_partialord(false)
+        .impl_debug(false)
+        .impl_partialeq(false)
+        // Blacklist C types that we plan to replace in Rust.
+        .blacklist_type("vec3_t")
+        // To prune unused bindings, explicitly declare the types we care about.
+        // bindgen will produce these types and their dependencies.
+        .whitelist_type("cl_entity_s")
+        .whitelist_type("cl_enginefuncs_s")
+        .whitelist_type("cl_clientfuncs_s")
+        .whitelist_type("user_msg_s")
+        // Format
+        .rustfmt_bindings(true)
+        // Use Rust enums instead of constants to represent enum variants.
+        .default_enum_style(EnumVariation::Rust { non_exhaustive: true })
+        // Finish the builder and generate the bindings.
+        .generate()
+        // Unwrap the Result and panic on failure.
+        .expect("Unable to generate bindings");
+
+    // Write the bindings to the $OUT_DIR/sdk.rs file.
+    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    bindings
+        .write_to_file(out_path.join("sdk.rs"))
+        .expect("Couldn't write bindings!");
+}
---
+++ sdk/wrapper.hpp
@@ -0,0 +1,93 @@
+#include <windows.h>
+
+typedef struct vec3_s {
+    float x, y, z;
+} vec3_t;
+
+#include "common\interface.h"
+#include "common\wrect.h"
+#include "common\cl_dll.h"
+#include "engine\cdll_int.h"
+#include "common\r_studioint.h"
+#include "common\com_model.h"
+#include "common\cl_entity.h"
+#include "common\ref_params.h"
+#include "pm_shared\pm_defs.h"
+#include "pm_shared\pm_movevars.h"
+#include "engine\studio.h"
+#include "parsemsg.h"
+#include "common\cvardef.h"
+#include "common\triangleapi.h"
+#include "common\pmtrace.h"
+#include "common\event_api.h"
+#include "common\r_efx.h"
+#include "common\entity_types.h"
+#include "common\screenfade.h"
+#include "common\net_api.h"
+
+#include "VGUI\VGUI_Frame.h"
+#include "VGUI\VGUI_Panel.h"
+#include "GameUI\IVGuiModuleLoader.h"
+#include "GameUI\IRunGameEngine.h"
+#include "GameUI\IGameUI.h"
+#include "GameUI\IGameConsole.h"
+#include "VGUI2\IHTML.h"
+#include "VGUI2\IPanel.h"
+#include "VGUI2\ISurface.h"
+#include "VGUI2\IEngineVGui.h"
+
+typedef struct cl_clientfuncs_s
+{
+	int(*Initialize) (cl_enginefunc_t *pEnginefuncs, int iVersion);
+	int(*HUD_Init) (void);
+	int(*HUD_VidInit) (void);
+	void(*HUD_Redraw) (float time, int intermission);
+	int(*HUD_UpdateClientData) (client_data_t *pcldata, float flTime);
+	int(*HUD_Reset) (void);
+	void(*HUD_PlayerMove) (struct playermove_s *ppmove, int server);
+	void(*HUD_PlayerMoveInit) (struct playermove_s *ppmove);
+	char(*HUD_PlayerMoveTexture) (char *name);
+	void(*IN_ActivateMouse) (void);
+	void(*IN_DeactivateMouse) (void);
+	void(*IN_MouseEvent) (int mstate);
+	void(*IN_ClearStates) (void);
+	void(*IN_Accumulate) (void);
+	void(*CL_CreateMove) (float frametime, struct usercmd_s *cmd, int active);
+	int(*CL_IsThirdPerson) (void);
+	void(*CL_CameraOffset) (float *ofs);
+	struct kbutton_s *(*KB_Find) (const char *name);
+	void(*CAM_Think) (void);
+	void(*V_CalcRefdef) (struct ref_params_s *pparams);
+	int(*HUD_AddEntity) (int type, struct cl_entity_s *ent, const char *modelname);
+	void(*HUD_CreateEntities) (void);
+	void(*HUD_DrawNormalTriangles) (void);
+	void(*HUD_DrawTransparentTriangles) (void);
+	void(*HUD_StudioEvent) (const struct mstudioevent_s *event, const struct cl_entity_s *entity);
+	void(*HUD_PostRunCmd) (struct local_state_s *from, struct local_state_s *to, struct usercmd_s *cmd, int runfuncs, double time, unsigned int random_seed);
+	void(*HUD_Shutdown) (void);
+	void(*HUD_TxferLocalOverrides) (struct entity_state_s *state, const struct clientdata_s *client);
+	void(*HUD_ProcessPlayerState) (struct entity_state_s *dst, const struct entity_state_s *src);
+	void(*HUD_TxferPredictionData) (struct entity_state_s *ps, const struct entity_state_s *pps, struct clientdata_s *pcd, const struct clientdata_s *ppcd, struct weapon_data_s *wd, const struct weapon_data_s *pwd);
+	void(*Demo_ReadBuffer) (int size, unsigned char *buffer);
+	int(*HUD_ConnectionlessPacket) (struct netadr_s *net_from, const char *args, char *response_buffer, int *response_buffer_size);
+	int(*HUD_GetHullBounds) (int hullnumber, float *mins, float *maxs);
+	void(*HUD_Frame) (double time);
+	int(*HUD_Key_Event) (int down, int keynum, const char *pszCurrentBinding);
+	void(*HUD_TempEntUpdate) (double frametime, double client_time, double cl_gravity, struct tempent_s **ppTempEntFree, struct tempent_s **ppTempEntActive, int(*Callback_AddVisibleEntity)(struct cl_entity_s *pEntity), void(*Callback_TempEntPlaySound)(struct tempent_s *pTemp, float damp));
+	struct cl_entity_s *(*HUD_GetUserEntity) (int index);
+	int(*HUD_VoiceStatus) (int entindex, qboolean bTalking);
+	int(*HUD_DirectorMessage) (unsigned char command, unsigned int firstObject, unsigned int secondObject, unsigned int flags);
+	int(*HUD_GetStudioModelInterface) (int version, struct r_studio_interface_s **ppinterface, struct engine_studio_api_s *pstudio);
+	void(*HUD_CHATINPUTPOSITION_FUNCTION) (int *x, int *y);
+	void(*CLIENTFACTORY) (void);
+	int(*HUD_GETPLAYERTEAM_FUNCTION) (int iplayer);
+} cl_clientfunc_t;
+
+typedef struct user_msg_s
+{
+	int iMsg;
+	int iSize;
+	char szName[16];
+	struct _UserMsg* next;
+	pfnUserMsgHook pfn;
+} user_msg_t;
