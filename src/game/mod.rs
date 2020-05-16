de609c0f1b946ffc86997511c90f324f659594b7
rkr35 <rkr35@drexel.edu>
2020-05-16 19:02:28 -0400

    Add name() on cl_entity_s

--- src/game/mod.rs
+++ src/game/mod.rs
@@ -7,11 +7,28 @@
 pub mod hw;
 pub mod vgui2;
 
+use std::ffi::CStr;
+
 use ultraviolet::Vec3 as vec3_t;
 include!(concat!(env!("OUT_DIR"), "/sdk.rs"));
 
 impl Clone for cl_clientfuncs_s {
     fn clone(&self) -> Self {
         unsafe { std::ptr::read(self) }
+    }
+}
+
+impl cl_entity_s {
+    pub fn name(&self) -> Option<&CStr> {
+        let model = self.model;
+        
+        if model.is_null() {
+            return None;
+        }
+        
+        Some(unsafe {
+            let name = (*model).name.as_ptr();
+            CStr::from_ptr(name)
+        })
     }
 }
